362d3a3521779af402d5165f554e633d716f90fe
Wlofe-shane <65356858+Wlofe-shane@users.noreply.github.com>
2025-05-30 16:32:18 -0400

    Seed Data Update

--- MvcMovie/Models/SeedData.cs
+++ MvcMovie/Models/SeedData.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Humanizer.Localisation;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MvcMovie.Data;
+using MvcMovie.Migrations;
 using System;
 using System.Linq;
 
@@ -52,6 +54,46 @@ namespace MvcMovie.Models
                         Genre = "Western",
                         Rating = "R",
                         Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Inception",
+                        ReleaseDate = DateTime.Parse("2010-7-16"),
+                        Genre = "Action",
+                        Rating = "R",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                    Title = "The Matrix",
+                        ReleaseDate = DateTime.Parse("1999-3-31"),
+                        Genre = "Science Fiction",
+                        Rating = "R",
+                        Price = 8.99M
+                    },
+                    new Movie
+                    {
+                    Title = "Interstellar",
+                        ReleaseDate = DateTime.Parse("2014-11-7"),
+                        Genre = "Science Fiction",
+                        Rating = "R",
+                        Price = 10.99M
+                    },
+                    new Movie
+                    {
+                    Title = "Parasite",
+                        ReleaseDate = DateTime.Parse("2019-5-30"),
+                        Genre = "Drama",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+                    new Movie
+                    {
+                    Title = "Spirited Away",
+                        ReleaseDate = DateTime.Parse("2001-7-20"),
+                        Genre = "Animation",
+                        Rating = "PG-13",
+                        Price = 6.99M
                     }
                 );
                 context.SaveChanges();
